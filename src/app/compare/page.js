3758ca83a2887802565f86761c92ef0a542ffc61
Matt Coffyn <matt.coffyn@gmail.com>
2024-01-09 21:44:38 +0000

    shuffle await calls

--- src/app/compare/page.js
+++ src/app/compare/page.js
@@ -1,6 +1,6 @@
 import { getUserData } from '../lib/userDataFetcher';
 import { getConsolidatedList } from '../lib/watchlistFetcher';
-import { CompareUserLists } from '../lib/compareUserLists';
+// import { CompareUserLists } from '../lib/compareUserLists';
 import WatchList from '../components/watchlist';
 import CompareList from '../components/compareList';
 
@@ -9,7 +9,7 @@ export default async function Compare(query) {
   const user2 = await getUserData(query.searchParams.friend2);
   const filmList1 = await getConsolidatedList(user1);
   const filmList2 = await getConsolidatedList(user2);
-  const matchList = await CompareUserLists(filmList1, filmList2);
+  // const matchList = await CompareUserLists(filmList1, filmList2);
 
   return (
     <div className="container grid grid-cols-1 lg:grid-cols-12 gap-1 lg:gap-6 mt-[-7rem] md:mt-[-4rem]">
@@ -21,7 +21,11 @@ export default async function Compare(query) {
         />
       </div>
       <div className="col-start-1 md:col-start-3 md:col-end-11 mt-16 p-8 rounded-md shadow-shorter bg-main">
-        <CompareList matchList={matchList} />
+        {/* <CompareList matchList={matchList} /> */}
+        <CompareList
+          list1={filmList1}
+          list2={filmList2}
+        />
       </div>
 
       <div className="col-end-13 col-span-2 pl-3 border-l border-gray-600 hidden lg:block">
