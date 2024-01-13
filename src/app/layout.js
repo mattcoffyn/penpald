d68eda77e4dfde22d8744d9ce162d2666da01734
Matt Coffyn <matt.coffyn@gmail.com>
2024-01-13 09:59:24 +0000

    trying out vercel speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Suspense } from 'react';
 import { NavigationEvents } from './components/navigationEvents';
 import MainHeader from './components/mainHeader';
@@ -45,6 +46,7 @@ export default function RootLayout({ children }) {
           </Suspense>
         </main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
